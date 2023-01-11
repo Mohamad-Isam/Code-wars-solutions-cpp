b29fe4f044db9b1dcd763d185c433ebf3fdc699f
Train XIII <116897631+MohammedIsamAli@users.noreply.github.com>
2023-01-11 19:29:35 +0200

    Create Basic Mathematical Operations.cpp

---
+++ level 8/Basic Mathematical Operations.cpp
@@ -0,0 +1,7 @@
+int basicOp(char op, int v1, int v2) {
+  switch(op){ 
+    case '+': return v1 + v2;
+    case '-': return v1 - v2;
+    case '*': return v1 * v2;
+    case '/': return v1 / v2;}
+}
