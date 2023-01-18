1192ec23daa3b8b3d3f2d1aa64396357ff1ecc6b
Train XIII <116897631+MohammedIsamAli@users.noreply.github.com>
2023-01-18 07:32:48 +0200

    Create Simple Fun #37: House Numbers Sum.cpp

---
+++ level 7/Simple Fun #37: House Numbers Sum.cpp
@@ -0,0 +1,8 @@
+int house_numbers_sum(const std::vector<int> &arr)
+{
+  int sum = 0;
+    for(int i = 0; i < arr.size(); i++)
+      if(arr[i] == 0) break;
+      else sum += arr[i];
+  return sum;
+}
