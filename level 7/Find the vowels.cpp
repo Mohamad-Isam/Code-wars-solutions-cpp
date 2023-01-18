8c8c6858358b36776739cd61f2615e02c4e79309
Train XIII <116897631+MohammedIsamAli@users.noreply.github.com>
2023-01-18 07:12:04 +0200

    Create Find the vowels.cpp

---
+++ level 7/Find the vowels.cpp
@@ -0,0 +1,9 @@
+#include <vector>
+#include <string>
+
+std::vector<int> vowelIndices(const std::string& word)
+{
+    std::vector<int> res;
+  for(int i = 0; i < word.size(); i++) if(word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || word[i] == 'o' || word[i] == 'u' || word[i] == 'y' || word[i] == 'A' || word[i] == 'E' || word[i] == 'I' || word[i] == 'O' || word[i] == 'U' || word[i] == 'Y') res.push_back(i + 1);
+    return res;
+}
